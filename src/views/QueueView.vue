f2f21e73191e2b3dfa7914a61cc05a09d8bc9e12
Roy Smith <roy@panix.com>
2024-08-23 13:46:05 -0400

    Add QueueView

---
+++ src/views/QueueView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="queue">
+      <h1>This is a queue</h1>
+      <button @click="getUserInfo">UserInfo!</button>
+      <p>
+        {{ data }}
+      </p>
+    </div>
+  </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const url = 'https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&prop=revisions&titles=Template:Did you know/Queue/3&formatversion=2&rvprop=content&rvslots=*';
+
+const data = ref(null);
+const error = ref(null);
+
+function getUserInfo() {
+  fetch(url)
+    .then((res) => res.text())
+    .then((text) => (data.value = text))
+    .catch((err) => (error.value = err))
+}
+
+onMounted(() => {
+  getUserInfo();
+})
+
+</script>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .queue {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
