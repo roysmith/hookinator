5aef28d8c6c85e3ea60c2ed935333acdcb17284e
Roy Smith <roy@panix.com>
2024-08-28 08:51:12 -0400

    Tear out AboutView

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/queue">Queue</RouterLink>
       </nav>
     </div>
